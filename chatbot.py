a8c6df8616fe807612e188073a9a33e3a44fb7bd
Shivam-dev-17 <singhshivam1703@gmail.com>
2026-06-16 21:37:58 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,40 +1,60 @@
 import random
+
 from responses import responses
-class chatbot:
-  def def_init_(self):
-     self.name="Nova"
-  def get_response(self, user_input):
-    
+
+
+class ChatBot:
+
+
+    def __init__(self):
+
+        self.name = "Nova"
+
+
+
+    def get_response(self, user_input):
+
         user_input = user_input.lower().strip()
 
-       #greetings
-       if user_input in ["hello", "hi", "hey"]:
+
+        # Greetings
+
+        if user_input in ["hello", "hi", "hey"]:
 
             return random.choice(
                 responses["greeting"]
             )
 
+
         # Asking bot condition
+
         elif "how are you" in user_input:
 
             return random.choice(
                 responses["how_are_you"]
             )
 
+
         # Asking name
+
         elif "your name" in user_input:
 
             return random.choice(
                 responses["name"]
             )
-          
+
+
         # Thanks
+
         elif "thank" in user_input:
 
             return random.choice(
                 responses["thanks"]
             )
 
+
         # Unknown input
+
         else:
+
             return "Sorry, I don't understand that yet."
