7b00d332611263299445379ff4f64454acfda13f
Shivam-dev-17 <singhshivam1703@gmail.com>
2026-06-16 11:48:27 +0530

    Create responses.py

---
+++ responses.py
@@ -0,0 +1,19 @@
+responses={
+  "greetings":["Hello! How can I help you?",
+                "Hi there! Nice to meet you.",
+                "Hey! What can I do for you?",
+                "How you Doin?"]
+  "how_are_you": ["I am doing great..as always!",
+                  "I am running smoothly!",
+                  "I am ready to assist you."],
+  "name": ["I am Nova, your AI assistant." ],
+"thanks": ["You're welcome!",
+           "Happy to help!",
+           "Anytime!"],
+"bye": ["Goodbye! Have a great day.",
+        "See you soon!",
+        "Take care!" ]
+
+}
+  
+  
