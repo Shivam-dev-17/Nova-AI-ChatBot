49ea282b48cbca694b86673709fb76b19de5b001
Shivam-dev-17 <singhshivam1703@gmail.com>
2026-06-16 15:01:28 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,40 @@
+import random
+from responses import responses
+class chatbot:
+  def_init_(self):
+     self.name="Nova"
+  def get_response(self, user_input):
+    
+        user_input = user_input.lower().strip()
+
+       #greetings
+       if user_input in ["hello", "hi", "hey"]:
+
+            return random.choice(
+                responses["greeting"]
+            )
+
+        # Asking bot condition
+        elif "how are you" in user_input:
+
+            return random.choice(
+                responses["how_are_you"]
+            )
+
+        # Asking name
+        elif "your name" in user_input:
+
+            return random.choice(
+                responses["name"]
+            )
+          
+        # Thanks
+        elif "thank" in user_input:
+
+            return random.choice(
+                responses["thanks"]
+            )
+
+        # Unknown input
+        else:
+            return "Sorry, I don't understand that yet."
