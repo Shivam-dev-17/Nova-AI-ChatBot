15a162eb5040f5fd0953cc5a9a1aa0989e7c6e20
Shivam-dev-17 <singhshivam1703@gmail.com>
2026-06-17 17:53:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from chatbot import ChatBot
+from gui import ChatBotGUI
 
-bot = ChatBot()
+app=ChatBotGUI()
 
-print("-"*40)
-print("🤖 Nova AI Chatbot")
-print("Type 'exit' to stop")
-print("-"*40)
-
-while True:
-    user_input = input("\nYou: ")
-    if user_input.lower()=="exit":
-        print("Bot: Goodbye Folk!")
-        break
-    response = bot.get_response(user_input)
-    print("Bot:", response)
+app.start()
